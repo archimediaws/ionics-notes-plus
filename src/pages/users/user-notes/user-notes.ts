861618ac46fd26b458848f5db8dbeec9269cc718
Stéphane ESCOBAR <contact@archimedia.ws>
2017-11-28 13:51:25 +0100

    update

--- src/pages/users/user-notes/user-notes.ts
+++ src/pages/users/user-notes/user-notes.ts
@@ -1,22 +1,44 @@
-
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, ModalController, NavController, NavParams, ViewController} from 'ionic-angular';
+import {UserProvider} from "../../../providers/user/user";
 
 @IonicPage()
 @Component({
   selector: 'page-user-notes',
   templateUrl: 'user-notes.html',
 })
-export class UserNotesPage  {
+export class UserNotesPage {
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider: UserProvider, public viewCtrl: ViewController, public modalCtrl: ModalController) {}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  
+  public user: any = null;
+  public notes: any[] = [];
+
+  /**
+   * Récupère la liste des notes d'un utilisateur
+   * @returns {Promise<any>}
+   */
+  ionViewCanEnter(): Promise <any> {
+    return new Promise((resolve, reject) => {
+      this.userProvider.getNotesByUserId(this.navParams.get('id')).subscribe(response => {
+        this.user = response['user'];
+        this.notes = response['notes'];
+        console.log(response);
+        resolve();
+      }, error => {
+        console.log(error);
+        reject();
+      });
+    });
   }
 
+  closeModal() {
+    this.viewCtrl.dismiss();
+  }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserNotesPage');
+  openNote(note) {
+    let modal = this.modalCtrl.create('NotePage', {note: note});
+    modal.present();
   }
 
 }
--- src/pages/users/users/users.ts
+++ src/pages/users/users/users.ts
@@ -1,28 +1,50 @@
-import {AuthService} from "../../../services/auth";
-import { AuthPage } from '../../auth/auth/auth';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {App, IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
+import {UserProvider} from "../../../providers/user/user";
 
 @IonicPage()
 @Component({
   selector: 'page-users',
   templateUrl: 'users.html',
 })
-export class UsersPage  {
+export class UsersPage {
 
- 
+  public users: any[] = [];
+  public count: number = 0;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public AuthService: AuthService) {
-    //super(AuthService, navCtrl)
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider: UserProvider, public modalCtrl: ModalController, public app: App) {}
+
+  ionViewCanEnter(): Promise <any> {
+    return new Promise((resolve, reject) => {
+      this.userProvider.getAll().subscribe(response => {
+        this.users = response['users'];
+        this.count = response['count'];
+        resolve();
+      }, error => {
+        console.log(error);
+        reject();
+      });
+    });
   }
 
-  ionViewCanEnter() {
-    //super.ionViewCanEnter();
+  loadMore() {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UsersPage');
+  goToUserPage(id) {
+    let modal = this.modalCtrl.create('UserNotesPage', {id: id});
+    modal.present();
   }
 
+  goToMyAccount() {
+    let modal = this.modalCtrl.create('AccountPage');
+    modal.present();
+
+
+    modal.onDidDismiss(data => {
+      if( data ) {
+        this.app.getRootNavs()[0].setRoot('LoginPage');
+      }
+    })
+  }
 }
