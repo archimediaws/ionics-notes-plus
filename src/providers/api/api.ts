94ab4cc806e1f75aa1abbb5b14190da1e56f391d
Stéphane ESCOBAR <contact@archimedia.ws>
2017-11-28 09:18:00 +0100

    alex api

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -13,7 +13,7 @@ import {Headers} from "@angular/http";
 export class ApiProvider {
 
   private token: string;
-  private apiUrl: string = 'http://developpeurwebapplimobile.com/';
+  private apiUrl: string = 'http://idem-api.alexandre-ribes.fr/';
 
   constructor(public http: HttpClient, public storage: Storage) {}
 
